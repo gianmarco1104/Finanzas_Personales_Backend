f2e6e54ef4374743ba99a1c1270ee024b937fc36
Gian Marco Mejia Torres <gianmarcomejiatorreswk@gmail.com>
2025-12-09 14:42:30 -0500

    First commit

---
+++ src/main/java/finances_practice/gmejia/GmejiaApplication.java
@@ -0,0 +1,13 @@
+package finances_practice.gmejia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmejiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmejiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finances_practice/gmejia/GmejiaApplicationTests.java
@@ -0,0 +1,13 @@
+package finances_practice.gmejia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmejiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
