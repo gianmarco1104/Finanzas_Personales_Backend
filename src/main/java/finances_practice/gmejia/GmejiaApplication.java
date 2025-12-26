c28812646975d30feb05ab7accf13a2c8cfddd54
Gian Marco Mejia Torres <gianmarcomejiatorreswk@gmail.com>
2025-12-26 14:07:33 -0500

    Ignorar .env en el Application

--- src/main/java/finances_practice/gmejia/GmejiaApplication.java
+++ src/main/java/finances_practice/gmejia/GmejiaApplication.java
@@ -12,12 +12,11 @@ public class GmejiaApplication {
     private String timeZone;
 
 	public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
-        //Carga el archivo .env
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // <--- ¡ESTA ES LA LÍNEA MÁGICA! ✨
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(GmejiaApplication.class, args);
 	}
 }
